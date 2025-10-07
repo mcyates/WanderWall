dd21e452d58dc9596f23beb3a921fcc8290e3e5c
Matthew Yates <Mat.C.Yates@gmail.com>
2025-10-07 13:48:05 -0400

    added Sass and a modern-normalize

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello World</h1>
+    <header>
+      <h1>WanderWall</h1>
+    </header>
     </>
   )
 }
